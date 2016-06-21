c345db25a184187d2c05005521bd431b467f470d
mahipal singh chundawat <mahipal033@gmail.com>
2016-06-21 11:35:28 +0530

    Update Sarv.php

--- Sarv.php
+++ Sarv.php
@@ -32,7 +32,7 @@ class Sarv {
         curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, 30);
         curl_setopt($this->ch, CURLOPT_TIMEOUT, 600);
 
-        $this->root = "http://$SarvTES_APP_DOMAIN:7279/v1.0/";
+        $this->root = "http://$SarvTES_APP_DOMAIN:7278/v1.0/";
         $this->messages = new Sarv_Messages($this);
         $this->settings = new Sarv_Settings($this);
         $this->accounts = new Sarv_Accounts($this);
